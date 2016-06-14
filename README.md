c6ef683246e0f5e81ddce251a441a292ab80d52d
carolineartz <ceartz@gmail.com>
2016-06-13 19:46:05 -0500

    Use hosted or local files.

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 This is a plugin for [FoldingText](http://www.foldingtext.com/) app.
 
-Adds @img tag support for lines with a markdown syntax image. Image will display on the following line. For now, images must be hosted and linked directly to the resource (not a page displaying it).
+Adds `@img` tag support for lines with a markdown syntax image.
 
-## Example
+## Examples
+### Hosted Images
+![types of links- hosted](hosted.png)
+### Local Files
+![types of links- local](local.png)
 ![my_cats](https://raw.githubusercontent.com/carolineartz/rendered_images_for_folding_text/master/rendered-images.png)
 
 ### TODO:
--- rendered_images.ftplugin/main.js
+++ rendered_images.ftplugin/main.js
@@ -1,19 +1,24 @@
 define(function(require, exports, module) {
-    'use strict';
+  'use strict';
 
-    var Extensions = require('ft/core/extensions').Extensions;
-    Extensions.addRenderNode(function(editor, node, nodeRenderer) {
-        if (node.tags() && node.tags().hasOwnProperty('img')) {
-            var image_widget = document.createElement('span');
-            image_widget.className = 'rendered-image';
-            var image = document.createElement("img");
-            var matches = node.text().match(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/);
-            if (matches) {
-                image.src = matches[0];
-                image_widget.appendChild(image);
-                nodeRenderer.renderLineWidget(image_widget);
-            }
-        }
-    });
+  var Extensions = require('ft/core/extensions').Extensions;
 
+  Extensions.addRenderNode(function(editor, node, nodeRenderer) {
+    if (node.tags() && node.tags().hasOwnProperty('img')) {
+      var matches = node.text().match(/(^|\s|-)\!\[(.+)\]\((.+)\)/);
+
+      if (matches) {
+        var image_widget, image;
+
+        image_widget = document.createElement('span');
+        image_widget.className = 'rendered-image';
+
+        image = document.createElement("img");
+        image.src = matches[3];
+        image_widget.appendChild(image);
+
+        nodeRenderer.renderLineWidget(image_widget);
+      }
+    }
+  });
 });
